6f0302868fd97f70cb342343704feee3fa8a4e34
Mayank Gupta <mayank.gupta@cube.ms>
2026-02-06 14:53:11 +0530

    Payment microservice code initialized

---
+++ apps/payment/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/payment/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(5004, () => {
+  console.log("Payment service is running on port 5004");
+});
