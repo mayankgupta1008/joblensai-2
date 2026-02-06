671f7758884420e6347fb8de0f72bdd1aed614d8
Mayank Gupta <mayank.gupta@cube.ms>
2026-02-06 17:10:42 +0530

    Frontend basic made, removed useless libraries

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,41 +1,5 @@
-import Navbar from "@/components/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { useSession } from "@/lib/auth-client";
-import DashboardPage from "@/pages/DashboardPage";
-import LoginPage from "@/pages/LoginPage";
-import SignupPage from "@/pages/SignupPage";
-import ForgotPassword from "@/pages/ForgotPassword";
-
-function App() {
-  const { data: session } = useSession();
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={session ? <DashboardPage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!session ? <SignupPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={session ? <Navigate to="/" /> : <LoginPage />}
-        />
-        <Route
-          path="/forgot-password"
-          element={!session ? <ForgotPassword /> : <Navigate to="/" />}
-        />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,20 +1,5 @@
-import logo from "@/assets/logo.svg";
-
 const Navbar = () => {
-  return (
-    <header className="bg-white border-b border-gray-200 px-6 py-4">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="w-8 h-8 bg-gradient-to-br from-blue-400 to-cyan-400 rounded-lg flex items-center justify-center">
-            <img src={logo} alt="Logo" />
-          </div>
-          <span className="text-xl font-semibold text-gray-900">
-            Weather Agent
-          </span>
-        </div>
-      </div>
-    </header>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
