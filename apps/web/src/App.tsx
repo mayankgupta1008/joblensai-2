68507586018700a60a9b717493771e62c7006e83
Mayank Gupta <mayank.gupta@cube.ms>
2026-02-07 15:54:56 +0530

    basic structure made.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,32 +1,5 @@
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
-import SwipeJobs from "./pages/SwipeJobs";
-import Settings from "./pages/Settings";
-import { AppLayout } from "@/components/AppLayout";
-import NotFound from "./pages/NotFound";
-
-const App = () => (
-  <TooltipProvider>
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/swipe" element={<SwipeJobs />} />
-
-        <Route element={<AppLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/applications" element={<Dashboard />} />
-          <Route path="/agent" element={<Dashboard />} />
-          <Route path="/profile" element={<Settings />} />
-          <Route path="/settings" element={<Settings />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </TooltipProvider>
-);
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
