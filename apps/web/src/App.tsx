4a3af61ab5a3e1af2c51707d8bea5b332caefc29
Mayank Gupta <mayank.gupta@cube.ms>
2026-02-07 15:58:21 +0530

    Basic structure made

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <LandingPage />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
