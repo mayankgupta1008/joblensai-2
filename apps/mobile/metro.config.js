ae7b4e88ebf119abac1fdc4bba1c0f013c7910b8
Mayank Gupta <mayankasheshgupta@gmail.com>
2026-08-19 15:38:42 +0530

    gitignore updated

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,12 +1,12 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 // withUniwindConfig must stay the outermost wrapper if more are added.
 module.exports = withUniwindConfig(config, {
-  cssEntryFile: './src/global.css',
-  dtsFile: './src/uniwind-types.d.ts',
+  cssEntryFile: "./src/global.css",
+  dtsFile: "./src/uniwind-types.d.ts",
 });
